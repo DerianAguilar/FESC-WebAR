77fa9fb065b89096a463b21ca34d7a230132436e
DerianAguilar <116276342+DerianAguilar@users.noreply.github.com>
2023-11-19 20:35:21 -0500

    version with interactive button that redirects to the program page

---
+++ js/project.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const askButtom = document.querySelector('#ask-button');
+    askButtom.addEventListener("click", event => {
+      window.location.href = "https://www.fesc.edu.co/micrositios/software/"
+    });
+  })
